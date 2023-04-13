8a15b16925ebb9c683b4b7786bd05c325ea80cef
brittaney <brittaney@upandcs.com>
2023-04-13 17:36:55 +0000

    Migrate material model

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Material(models.Model):
+    material_id = models.AutoField(
+        primary_key=True,
+        editable=False,
+        verbose_name='Material ID',
+        db_column='material_id',
+    )
+    description = models.CharField(
+        max_length=255,
+        verbose_name='Description',
+        db_column='description',
+    )
+    manufacturer = models.CharField(
+        max_length=255,
+        verbose_name='Manufacturer',
+        db_column='manufacturer',
+    )
+    manufacturer_number = models.CharField(
+        max_length=255,
+        verbose_name='Manufacturer Number',
+        db_column='manufacturer_number',
+    )
+
+    class Meta:
+        db_table = 'materials'
+        verbose_name = 'Material'
+        verbose_name_plural = 'Materials'
+        ordering = ['description']
+
+    def __str__(self):
+        return self.description
