48f92296c1be83b27533f15f231ab3b323d16cac
brittaney <brittaney@upandcs.com>
2023-04-13 16:32:16 +0000

    Migrate resource status model

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ResourceStatus 
 
-# Register your models here.
+
+admin.site.register(ResourceStatus)
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ResourceStatus(models.Model):
+    resource_status_id = models.AutoField(
+        primary_key=True,
+        editable=False,
+        verbose_name='Resource Status ID',
+        db_column='resource_status_id',
+    )
+    resource_status = models.CharField(
+        max_length=30,
+        verbose_name='Resource Status',
+        db_column='resource_status',
+    )
+
+    class Meta:
+        db_table = 'resource_statuses'
+        verbose_name = 'Resource Status'
+        verbose_name_plural = 'Resource Statuses'
+        ordering = ['resource_status']
+
+    def __str__(self):
+        return self.resource_status
