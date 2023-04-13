da13608b9e917cddf177587a40ecc109c6da34cc
brittaney <brittaney@upandcs.com>
2023-04-13 17:10:09 +0000

    Migrate site type model

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteType(models.Model):
+    site_type_id = models.AutoField(
+        primary_key=True,
+        editable=False,
+        verbose_name='Site Type ID',
+        db_column='site_type_id',
+    )
+    site_type = models.CharField(
+        max_length=50,
+        verbose_name='Site Type',
+        db_column='site_type',
+    )
+
+    class Meta:
+        db_table = 'site_types'
+        verbose_name = 'Site Type'
+        verbose_name_plural = 'Site Types'
+        ordering = ['site_type']
+
+    def __str__(self):
+        return self.site_type
