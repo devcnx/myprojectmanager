df252b51d548be106bf6bc8682a7e52b0d08898d
brittaney <brittaney@upandcs.com>
2023-04-13 18:31:22 +0000

    Migrate equipment model

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Equipment 
 
-# Register your models here.
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('equipment_id', 'description')
+    search_fields = ('description',)
+    list_filter = ('description',)
+    ordering = ('equipment_id',)
+    list_per_page = 25
+
+
+admin.site.register(Equipment, EquipmentAdmin)
--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipment(models.Model):
+    equipment_id = models.AutoField(
+        primary_key=True,
+        editable=False,
+        verbose_name='Equipment ID',
+        db_column='equipment_id',
+    )
+    description = models.CharField(
+        max_length=255,
+        verbose_name='Description',
+        db_column='description',
+    )
+
+    class Meta:
+        db_table = 'equipment'
+        verbose_name = 'Equipment'
+        verbose_name_plural = 'Equipment'
+        ordering = ['equipment_id']
+
+    def __str__(self):
+        return self.description
